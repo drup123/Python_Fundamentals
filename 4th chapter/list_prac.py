0ca4a40415e8203e09896c386be2fe40caf40280
Drup Patil <145984896+drup123@users.noreply.github.com>
2025-06-07 14:17:21 +0530

    Add files via upload

---
+++ 4th chapter/list_prac.py
@@ -0,0 +1,13 @@
+list = ["aman","arun","drup"]
+
+i = input("enter your name : ")
+if(i in list):
+    print("name is in list")
+else:
+    print("name in list")
+
+
+u = "drup patil"
+
+if('rup' in u):
+    print("yes")
---
+++ 4th chapter/practice.py
@@ -0,0 +1,11 @@
+all = int(input("enter the overall marks :"))
+
+sub1 = int(input("enter marks of 1st sub :"))
+sub2 = int(input("enter marks of 2nd sub :"))
+sub3 = int(input("enter marks of 3rd sub :"))
+
+if(all>40 and sub1,sub2,sub3>33):
+    print("Pass")
+else:
+    print("Fail")
+
